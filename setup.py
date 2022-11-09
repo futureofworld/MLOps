9465715f10a0fcd601f2c388d6b3b2bc8471151b
Rahul <rdhaval5720@gmail.com>
2022-11-09 12:38:05 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="It's a Wine Quality Package",
+    author="Rahul",
+    packages=find_packages(),
+    license="MIT"
+)
